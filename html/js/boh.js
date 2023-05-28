f5363ad6e44099e0bcefefdafab092ec81335743
VieriF <128815046+VieriF@users.noreply.github.com>
2023-05-28 12:48:45 +0200

    creato il progetto

---
+++ html/js/boh.js
@@ -0,0 +1,15 @@
+console.log("ciaov");
+
+var primoP = document.getElementById("primoP");
+var secondoP = document.getElementById("secondoP");
+var terzoP = document.getElementById("terzoP");
+var quartoP = document.getElementById("quartoP");
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var img3 = document.getElementById("img3");
+var img4 = document.getElementById("img4");
+
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
